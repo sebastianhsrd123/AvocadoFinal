0ba5287f2ecb78e76655c926174ef3d33a8484de
unknown <hugo2171910@correo.uis.edu.co>
2022-09-15 00:44:20 -0500

    Añadido carrito y vinculado al API al 100%

--- src/app/data/constants/slider.const.ts
+++ src/app/data/constants/slider.const.ts
@@ -3,42 +3,42 @@ import { ISliderItem} from '@shared/components/slider/islider-item.metadata';
 export const SLIDER_ITEMS: ISliderItem[] = [
     {
     id:1,
-    title: "LOREM IPSUM 1",
-    subtitle: "Nicolas care verga bostoniana .l.",
+    title: "DEL CAMPO A TU CASA",
+    subtitle: "La vida en la ciudad no es vida para un hombre de campo",
     link: "/",
     image: "./assets/img/1.jpg",
-    
+
     },
     {
     id:2,
-    title: "LOREM IPSUM 2",
-    subtitle: "Nicolas care verga bostoniana .l.",
+    title: "PRODUCTOS FRESCOS",
+    subtitle: "Crecí en el campo y estaba obsesionada con los caballos y la vida salvaje",
     link: "/",
     image: "./assets/img/2.jpg",
-    
+
     },
     {
     id:3,
-    title: "LOREM IPSUM 3",
-    subtitle: "Nicolas care verga bostoniana .l.",
+    title: "SOMOS LA MEJOR OPCION",
+    subtitle: "El campo es lírico, la ciudad dramática. Cuando se mezclan, hacen el drama musical más perfecto",
     link: "/",
     image: "./assets/img/3.jpg",
-    
-    }, 
+
+    },
     {
     id:4,
-    title: "LOREM IPSUM 4",
-    subtitle: "Nicolas care verga bostoniana .l.",
+    title: "TENEMOS COMPROMISO",
+    subtitle: "Dos cosas revolucionaron mi vida: mudarme al campo y enamorarme",
     link: "/",
     image: "./assets/img/4.jpg",
-    
+
     },
     {
     id:5,
-    title: "LOREM IPSUM 5",
-    subtitle: "Nicolas care verga bostoniana .l.",
+    title: "UNA ALIMENTACION SANA",
+    subtitle: "Yo viví en la soledad del campo y noté cómo la monotonía de una vida tranquila estimula la mente creativa",
     link: "/",
     image: "./assets/img/5.jpg",
-    
+
     }
 ]
